fcb8760103824af4da782bbeea5aef612f55b281
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-05 21:42:34 -0600

    Thank you page added

--- routes/contact.js
+++ routes/contact.js
@@ -2,7 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-    res.render("contact", { title: "Any suggestion?" })
+  res.render("contact", { title: "Any suggestion?" });
+});
+
+router.post("/", (req, res, next) => {
+  res.render("thanks", {
+    title:
+      "Thanks for contacting us, we'll get in touch really soon! Any feedback is greatly appreciated!",
+  });
 });
 
 module.exports = router;
