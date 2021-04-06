a678844dc12bfa00bc5872f872fdf5ae028a6d36
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-06 17:24:11 -0600

    Authentication forms added

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,11 @@ const router = express.Router();
 
 //authentication routes
 router.get("/login", (req, res, next) => {
-  res.render("auth/login", { title: "login" });
+  res.render("auth/login", { title: "Login" });
 });
 
 router.get("/register", (req, res, next) => {
-  res.render("auth/register", { title: "register" });
+  res.render("auth/register", { title: "Register" });
 });
 
 module.exports = router;
