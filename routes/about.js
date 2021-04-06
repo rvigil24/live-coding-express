9784446a8cd169c5a3d46b11b047ed64344d3394
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-05 21:43:07 -0600

    changed name to RT-Coding

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("about", { title: "About Code Living" });
+  res.render("about", { title: "About RT-Coding" });
 });
 
 module.exports = router;
