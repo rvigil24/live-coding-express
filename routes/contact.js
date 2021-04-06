6396561454048abdfa2678d582c3eb9b83f73794
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-06 02:27:14 -0600

    Form validation added

--- routes/contact.js
+++ routes/contact.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const router = express.Router();
 
+//contact page
 router.get("/", (req, res, next) => {
   res.render("contact", { title: "Any suggestion?" });
 });
 
-router.post("/", (req, res, next) => {
-  res.render("thanks", {
-    title:
-      "Thanks for contacting us, we'll get in touch really soon! Any feedback is greatly appreciated!",
-  });
-});
+//thank you page
+router.post(
+  "/",
+  body("name", "name field is required").notEmpty(),
+  body("email", "email field is required").isEmail(),
+  body("message", "message is required").notEmpty(),
+  (req, res, next) => {
+    const { name, email, message } = req.body;
+    const errors = validationResult(req);
+    return !errors.isEmpty()
+      ? res.render("contact", {
+          title: "Any suggestion?",
+          name,
+          email,
+          message,
+          errors: errors.array(),
+        })
+      : res.render("thanks", {
+          title: "Thanks for contacting us!",
+        });
+  }
+);
 
 module.exports = router;
