b99994852564b1aa252a131f23c837887a01ac9b
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-05 21:31:48 -0600

    about and contact us routes added to app.js

--- app.js
+++ app.js
@@ -3,16 +3,20 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const hbs = require("hbs");
 
 //import our routes
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const aboutRouter = require("./routes/about");
+const contactRouter = require("./routes/contact");
 
 const app = express();
 
 // view engine setup, we are using handlebars this time
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -20,9 +24,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-//our routes
+//our routes go here
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/about", aboutRouter);
+app.use("/contact", contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
